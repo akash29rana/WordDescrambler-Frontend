2a29b739c8617866deba7318d9150a360ca315fc
AKASH RANA <akashrana@AKASHs-Laptop.local>
2025-06-17 01:18:47 +0530

    add hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'; // Use HashRouter
 import Menu from './components/Menu';
 import WordUnscrambler from './pages/WordUnscrambler';
 import Unscramble from './pages/Unscramble';
