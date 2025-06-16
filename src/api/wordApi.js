2f9927f80a767a2a2f5516eae8e5eef464e4cab3
AKASH RANA <akashrana@AKASHs-Laptop.local>
2025-06-17 01:09:02 +0530

    changes

--- src/api/wordApi.js
+++ src/api/wordApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-//const BASE_URL = 'https://api.wordsdescrambler.com'; // Update this to your FastAPI endpoint
-const BASE_URL = 'http://localhost:8000'; // Local development URL, change as needed
+const BASE_URL = 'https://api.wordsdescrambler.com'; // Update this to your FastAPI endpoint
+//const BASE_URL = 'http://localhost:8000'; // Local development URL, change as needed
 
 export const findWords = async (queryParams) => {
   const res = await axios.get(`${BASE_URL}/unscramble?${queryParams}`);
